5c63a705ca37af5c6bac2984129b6952954dbec6
Amr A. Abdelmoez <amradel88@live.com>
2018-04-02 06:14:35 +0200

    feat: wider canvas ( 7 columns )

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 const Player = function () {
-    this.x = 202;
+    this.x = 303;
     this.y = 405;
     this.sprite = 'images/char-boy.png';
 }
@@ -40,9 +40,9 @@ Player.prototype.render = function() {
 
 Player.prototype.handleInput = function(direction) {
     if (direction === 'left') {
-        this.x === 0 ? this.x = 404 : this.x -= 101;
+        this.x === 0 ? this.x = 606 : this.x -= 101;
     } else if (direction === 'right') {
-        this.x === 404 ? this.x = 0 : this.x += 101;
+        this.x === 606 ? this.x = 0 : this.x += 101;
     } else if (direction === 'up' && this.y !== -10) {
         this.y -= 83;
     } else if (direction === 'down' && this.y !== 405) {
