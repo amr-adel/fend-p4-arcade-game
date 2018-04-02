b193513b9c41bd7d8645d9a1d81867262c920e78
Amr A. Abdelmoez <amradel88@live.com>
2018-04-02 23:35:10 +0200

    feat: change enemies speed randomly

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 const Enemy = function(y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = 202;
+    this.x = -101;
     this.y = y;
+    this.speed = 150 * (Math.floor(Math.random() * 5) + 1);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -15,6 +16,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.speed;
+    
+    if (this.x > 808) {
+        this.x = -101;
+        this.speed = 150 * (Math.floor(Math.random() * 5) + 1);
+    }
+    
 };
 
 // Draw the enemy on the screen, required method for game
