bc5fe6a8fb17c63910b71a9126091b6c2f12f1b6
Amr A. Abdelmoez <amradel88@live.com>
2018-04-05 04:46:29 +0200

    refactor: refactoring code

--- js/app.js
+++ js/app.js
@@ -1,103 +1,187 @@
-const Enemy = function(y) { // Enemies constructor function
-    this.freeToMove = true; // All enemies are free to move by default
-    this.x = -101; // Enemy start position is 1 block before far left
-    this.y = y; // Enemy vertical position
-    this.speed = 150 * (Math.floor(Math.random() * 5) + 1); // Assign enemy speed randomly
-    this.sprite = 'images/enemy-bug.png'; // Enemy character image
+// Global variables
+const columnWidth = 101;   // Single block width
+const rowHeight = 83;      // Single block height
+const bottomRow = 405;     // The bottom most row on canvas
+
+
+// Enemies constructor function
+const Enemy = function(y) {
+    // All enemies are free to move by default
+    this.freeToMove = true;
+    
+    // Enemy start position is 1 block before far left
+    this.x = -columnWidth;
+    
+    // Enemy vertical position
+    this.y = y;
+    
+    // Assign enemy speed randomly
+    this.speed = 150 * (Math.floor(Math.random() * 5) + 1);
+    
+    // Enemy character image
+    this.sprite = 'images/enemy-bug.png';
 };
 
-Enemy.prototype.update = function(dt) { // Update enemies objects
+
+// Update enemies objects
+Enemy.prototype.update = function(dt) {
     
     if (this.freeToMove) { // Move enemy only if it's free to move (not freezed)
         this.x += dt * this.speed;
     }
     
-    if (this.x > 808) { // When enemy reach far right, start again from left and assign new random speed
-        this.x = -101;
+     // If enemy has reached far right, start again from the left and assign new random speed
+    if (this.x > (columnWidth * 8)) {
+        this.x = -columnWidth;
         this.speed = 150 * (Math.floor(Math.random() * 5) + 1);;
     }
     
-    if (this.y === (player.y - 13) // When an enemy and the player are too close
+    // If an enemy and the player are too close
+    if (this.y === (player.y - 13)
         && this.x > (player.x - 80)
         && this.x < (player.x + 60)) {
-        freeze(this); // Freeze enemy
-        freeze(player); // Freeze player
-        message(lose); // Show the "lose" message
+        // Freeze enemy
+        freeze(this);
+        
+        // Freeze player
+        freeze(player);
+        
+        // Show the "lose" message
+        message(lose);
     }
 };
 
-Enemy.prototype.render = function() { // Draw the enemy on the screen
+
+// Draw enemy on screen
+Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-const Player = function () { // Player constructor function
-    this.freeToMove = true; // Player free to move by default
-    this.x = 303; // Start at 4th column
-    this.y = 405; // Start at bottom row
-    this.sprite = 'images/char-boy.png'; // Player character image
+
+// Player constructor function
+const Player = function () {
+    // Player is free to move by default
+    this.freeToMove = true;
+    
+    // Start at 4th column
+    this.x = columnWidth * 3;
+    
+    // Start at bottom most row
+    this.y = bottomRow;
+    
+    // Player character image
+    this.sprite = 'images/char-boy.png';
 }
 
-Player.prototype.update = function() { // Update player object
-    if (this.y === -10) { // When player reach the other side successfully
-        freeze(this); // Freeze player
-        message(win); // Show the "win" message
+
+// Update player object
+Player.prototype.update = function() {
+    // If player has reached the winning row successfully
+    if (this.y === -10) {
+        // Freeze player
+        freeze(this);
+        
+        // Show the "win" message
+        message(win);
     }
 };
 
-Player.prototype.render = function() { // Draw the player on the screen
+
+// Draw player on screen
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(direction) { // Move player character according to the pressed key
+
+// Move player character according to the pressed key
+Player.prototype.handleInput = function(direction) {
     if (this.freeToMove) { // Only if the player is free to move (not freezed)
+        
         if (direction === 'left') {
-            this.x === 0 ? this.x = 606 : this.x -= 101; // Start from far right when player is at far left and left key pressed
+            // Start from far right when player is at far left and the left key pressed
+            this.x === 0 ? this.x = (columnWidth * 6) : this.x -= columnWidth;
+            
         } else if (direction === 'right') {
-            this.x === 606 ? this.x = 0 : this.x += 101; // Start from far left when player is at far right and right key pressed
+            // Start from far left when player is at far right and the right key pressed
+            this.x === (columnWidth * 6) ? this.x = 0 : this.x += columnWidth;
+            
         } else if (direction === 'up') {
-            this.y -= 83; // Move player one block up
-        } else if (direction === 'down' && this.y !== 405) {
-            this.y += 83; // Move player one block down
+            // Move player one row up
+            this.y -= rowHeight;
+            
+        } else if (direction === 'down' && this.y !== bottomRow) {
+            // Move player one row down
+            this.y += rowHeight;
         }  
     }
 };
 
-const allEnemies = [ // Generating new enemies
-    new Enemy(60), // Top row enemy
-    new Enemy(143), // middle row enemy
-    new Enemy(226) // bottom row enemy
+
+// Generating new enemies
+const allEnemies = [
+    // Top track enemy
+    new Enemy(60),
+    
+    // middle track enemy
+    new Enemy(143),
+    
+    // bottom track enemy
+    new Enemy(226)
 ];
 
-const player = new Player(); // Generate new player
 
-document.addEventListener('keyup', function(e) { // Listen to keyboard arrow keys strokes
+// Generate new player
+const player = new Player();
+
+
+// Listen to keyboard arrow keys strokes
+document.addEventListener('keyup', function(e) {
     const allowedKeys = {
-        37: 'left', // left keyboard arrow key
-        38: 'up', // up keyboard arrow key
-        39: 'right', // right keyboard arrow key
-        40: 'down' // down keyboard arrow key
+        // left keyboard arrow key
+        37: 'left',
+        
+        // up keyboard arrow key
+        38: 'up',
+        
+        // right keyboard arrow key
+        39: 'right',
+        
+        // down keyboard arrow key
+        40: 'down'
     };
 
     player.handleInput(allowedKeys[e.keyCode]); 
 });
 
 
-function freeze(obj) { // Freeze object for some time, then release
-    obj.freeToMove = false; // Freeze object
+// Freeze object for some time, then release
+function freeze(obj) {
+    // Freeze object
+    obj.freeToMove = false;
+    
     setTimeout(function () {
-        if (obj === player) { // Reset player position
-            player.x = 303;
-            player.y = 405;
-        }
-        obj.freeToMove = true; // Release object
+        // Release object
+        obj.freeToMove = true;
+        
+        if (obj === player) { // Reset player position after release
+            player.x = columnWidth * 3;
+            player.y = bottomRow;
+        };
     }, 750);
 }
 
-function message(state) { // Show message according to state
-    state.style.display = 'inline-block'; // show message
-    setTimeout(function() { // Remove message with animation after some time
+
+// Show message according to state
+function message(state) {
+    // show message
+    state.style.display = 'inline-block';
+    
+    // Remove message with animation after some time
+    setTimeout(function() {
         state.classList.add('zoomOutDown');
-        setTimeout(function() { // Hide message and remove animation class to use it again
+        
+        // Hide message and remove animation class to use it again
+        setTimeout(function() {
             state.classList.remove('zoomOutDown');
             state.style.display = 'none';
         }, 750)
